a8dd667aae8c4883ccc1b5bd43a2b940c2a369a2
wdh2019 <18302010012@fudan.edu.cn>
2021-04-19 10:11:17 +0800

    init project
    1.vue init webpack project_name
    2.import vuex, axios, element-ui, three.js

---
+++ web_pj_frontend/src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="app">
+    <!-- <img src="./assets/logo.png"> -->
+    <router-view/>
+  </div>
+</template>
+
+<script>
+// 引入three.js
+import * as Three from 'three'	
+export default {
+  name: 'App'
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
